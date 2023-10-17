0b0858aedc1203df6f31bfba907f196708cbc03d
f3j43fz <129746748+f3j43fz@users.noreply.github.com>
2023-10-17 12:05:43 +0800

    Create install_docker_debian.sh

---
+++ install_docker_debian.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# install sudo
+apt install sudo -y
+
+# Uninstall old versions
+sudo apt-get remove -y docker.io docker-doc docker-compose podman-docker containerd runc
+
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+# Install Docker packages
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify the installation
+sudo docker run hello-world
